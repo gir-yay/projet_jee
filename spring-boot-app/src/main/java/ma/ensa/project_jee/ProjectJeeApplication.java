f336edbf54dd91ad169e826e23dacaf03b38ae9f
salmanben <salmanbenomar250@gmail.com>
2024-10-19 16:37:58 +0100

    Exclude security

--- spring-boot-app/src/main/java/ma/ensa/project_jee/ProjectJeeApplication.java
+++ spring-boot-app/src/main/java/ma/ensa/project_jee/ProjectJeeApplication.java
@@ -2,6 +2,7 @@ package ma.ensa.project_jee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class ProjectJeeApplication {

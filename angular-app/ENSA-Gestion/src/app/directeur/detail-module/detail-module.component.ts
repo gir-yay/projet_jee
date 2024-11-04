ae1b7dc4a734923877fcd8c75a312fbfe34d0857
ghizlane rahmouni <ghlaneiz022@gmail.com>
2024-11-04 03:17:24 +0100

    module detail unlinked

--- angular-app/ENSA-Gestion/src/app/directeur/detail-module/detail-module.component.ts
+++ angular-app/ENSA-Gestion/src/app/directeur/detail-module/detail-module.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormationComponent } from "../formation/formation.component";
 
 @Component({
   selector: 'app-detail-module',
   standalone: true,
-  imports: [],
+  imports: [FormationComponent],
   templateUrl: './detail-module.component.html',
   styleUrl: './detail-module.component.css'
 })

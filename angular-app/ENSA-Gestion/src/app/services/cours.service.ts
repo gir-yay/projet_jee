71648ed2523d8f93da5fc5363a4e093626615ba4
salmanben <salmanbenomar250@gmail.com>
2024-11-04 01:17:22 +0100

    Fix issue

--- angular-app/ENSA-Gestion/src/app/services/cours.service.ts
+++ angular-app/ENSA-Gestion/src/app/services/cours.service.ts
@@ -12,14 +12,14 @@ export class CoursService {
 
 
   /*========================================= Les Cours======================= */
-  
+
   // Cours par ID de Matieres
   getCoursByMatiereId(matiereId: number): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseApiUrl}/matiere/${matiereId}/cours`); 
+    return this.http.get<any[]>(`${this.baseApiUrl}/matiere/${matiereId}/cours`);
   }
   //ajouter cour
   ajouterCour(cours: any, matiereId: number): Observable<any> {
-    const data = { nom: cours.nom, matiereId: matiereId }; 
+    const data = { nom: cours.nom, matiereId: matiereId };
     return this.http.post(`${this.baseApiUrl}/cours/ajouter`, data);
   }
    // cours par ID
@@ -28,17 +28,17 @@ export class CoursService {
   }
    //Modifier un cour
    updateCour(courId: number, courData: any): Observable<any> {
-    return this.http.post(`${this.baseApiUrl}/cours/modifier/${courId}`, courData); 
+    return this.http.post(`${this.baseApiUrl}/cours/modifier/${courId}`, courData);
   }
   // Supprimer un Cours
   deleteCour(courId: number): Observable<any> {
-    return this.http.delete(`${this.baseApiUrl}/cours/${courId}`); 
+    return this.http.delete(`${this.baseApiUrl}/cours/${courId}`);
   }
 /*=================================================================================================================== */
   /*========================================= Les Liens======================= */
   // liens par ID
   getLiensByCoursId(coursId: number): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseApiUrl}/cours/${coursId}/liens`); 
+    return this.http.get<any[]>(`${this.baseApiUrl}/cours/${coursId}/liens`);
   }
   // ajouter un lien
   ajouterLien(lien: string, coursId: number): Observable<any> {
@@ -51,21 +51,21 @@ export class CoursService {
   }
   // modifier un lien
   updatelien(lienId: number, lienData: any): Observable<any> {
-    return this.http.post(`${this.baseApiUrl}/cours/liens/modifier/${lienId}`, lienData); 
+    return this.http.post(`${this.baseApiUrl}/cours/liens/modifier/${lienId}`, lienData);
   }
 /*=================================================================================================================== */
   /*========================================= Les Documents======================= */
-  
+
   // documents par ID
   getDocumentsByCoursId(coursId: number): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseApiUrl}/cours/${coursId}/documents`); 
+    return this.http.get<any[]>(`${this.baseApiUrl}/cours/${coursId}/documents`);
   }
-  //ajouter document 
+  //ajouter document
   ajouterDocument(nom: string, courId: number, file: File): Observable<any> {
     const formData = new FormData();
     const documentData = {
         nom: nom,
-        coursId: courId 
+        coursId: courId
     };
     console.log('Document Data:', documentData);
     formData.append('data', new Blob([JSON.stringify(documentData)], { type: 'application/json' }));
@@ -84,9 +84,12 @@ export class CoursService {
       coursId: courId
     };
     formData.append('data', new Blob([JSON.stringify(documentData)], { type: 'application/json' }));
-    
+
     if (file) {
       formData.append('file', file); // Ajoute le fichier si disponible
+    }
+    else{
+      formData.append('file', new File([], ''));
     }
 
     return this.http.post(`${this.baseApiUrl}/cours/documents/modifier/${id}`, formData);
